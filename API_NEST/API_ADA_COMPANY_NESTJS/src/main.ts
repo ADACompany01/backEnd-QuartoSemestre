b890957254a95ec61494a1fb862c55a20855357e
luizriato <142940208+luizriato@users.noreply.github.com>
2025-11-03 16:52:23 -0300

    fix(main.ts): correcao de cors

--- API_NEST/API_ADA_COMPANY_NESTJS/src/main.ts
+++ API_NEST/API_ADA_COMPANY_NESTJS/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-   origin: ['http://localhost:3000', 'https://newadacompany.vercel.app'], // ajuste os domínios
+   origin: ['http://localhost:80'], // ajuste os domínios
    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
    credentials: true,
   });
